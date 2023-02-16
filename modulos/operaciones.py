29ebd17b54ff804f371acf5a699cb15481eae283
Adria Torres <torres_ra@hotmail.com>
2023-02-15 20:33:59 -0600

    Importando modulos

---
+++ modulos/operaciones.py
@@ -0,0 +1,15 @@
+def suma(a,b):
+    '''
+    Devuelve la suma de dos numeros
+    
+    '''
+    return a + b
+
+def resta(a,b):
+    return a - b
+
+def multi(a,b):
+    return a - b
+
+def division(a,b):
+    return a - b
---
+++ modulos/resultados.py
@@ -0,0 +1,13 @@
+import operaciones as ops
+
+sum = ops.suma(10, 12)
+print('El resultado de la suma es: ', sum)
+
+res = ops.resta(80, 35)
+print('El resultado de la resta es: ', res)
+
+mult = ops.multi(35, 9)
+print('El resultado de la multiplicacion es: ', mult)
+
+div = ops.division(100, 25)
+print('El resultado de la division es: ', div)
